231842ec04079fa22ac40f10aaac7d499522fcdd
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-01-04 10:59:05 +0200

    Build SplashScreen
    - Install lottie package and setup lottie animations.

---
+++ lib/features/spash_screen/view.dart
@@ -0,0 +1,37 @@
+import 'package:clothes_e_shop/features/login/view.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+
+    Future.delayed(Duration(seconds: 3), () async {
+      if (mounted) {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return LoginView();
+            },
+          ),
+        );
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Lottie.asset("assets/animations/loading_shopping_cart.json"),
+      ),
+    );
+  }
+}
